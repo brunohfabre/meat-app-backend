5f1b666a01abb12fcd8fc9994747439366c9173b
Bruno Henrique de Fabre <brunofabre@iMac-de-Bruno.local>
2017-06-26 13:11:25 -0300

    add auth service

--- config/routes.js
+++ config/routes.js
@@ -1,20 +1,36 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-    //API Routes
-    const router = express.Router()
-    server.use('/api', router)
+    /*
+     * Rotas abertas
+     */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
+    /*
+     * Rotas protegidas por Token JWT
+     */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+	protectedApi.use(auth)
 
     //Rotas restaurants
     const restaurantService = require('../api/restaurant/restaurantService')
-    restaurantService.register(router, '/restaurants')
+    restaurantService.register(protectedApi, '/restaurants')
 
     //Rotas menus
     const menuService = require('../api/menu/menuService')
-    menuService.register(router, '/menus')
+    menuService.register(protectedApi, '/menus')
 
     //Rotas reviews
     const reviewService = require('../api/review/reviewService')
-    reviewService.register(router, '/reviews')
+    reviewService.register(protectedApi, '/reviews')
 }
